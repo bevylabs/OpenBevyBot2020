902147fa1e70a523e9e487e73b4f77dc2d35fa90
Buddhika P. De Silva <bevylabs@gmail.com>
2019-06-13 12:36:25 +0530

    set the UI

--- Android Apps/MathsGame/app/src/main/java/org/bevylabs/mathsgame/MainActivity.java
+++ Android Apps/MathsGame/app/src/main/java/org/bevylabs/mathsgame/MainActivity.java
@@ -2,6 +2,9 @@ package org.bevylabs.mathsgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView num1Txt=findViewById(R.id.textView);
+        TextView num2Txt=findViewById(R.id.textView4);
+        EditText resultTxt=findViewById(R.id.editText);
+
+
+    }
+
+    public void check(View view) {
 
     }
 }
